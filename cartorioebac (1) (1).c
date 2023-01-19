687aa2b796506de93ca9cbcdc8bdcf26817b24c8
saniellydev <123004483+saniellydev@users.noreply.github.com>
2023-01-19 11:32:03 -0300

    Retirada da opçao de saida
    Retirada da quarta opção de saída como ordenado pelo cliente

--- cartorioebac (1) (1).c
+++ cartorioebac (1) (1).c
@@ -112,8 +112,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
-		printf("\t4 - Sair do sistema\n\n");
-		printf("Opçao: ");//fim do menu
+        printf("Opçao: ");//fim do menu
 				
 		scanf("%d", &opcao); //armazenando a escolha do usuario
 		
@@ -131,11 +130,6 @@ int main()
 		
 			case 3:
 			deletar();
-			break;
-			
-			case 4:
-			printf("Obrigado por utilizar o sistema\n");
-			return 0;
 			break;
 		
 		
